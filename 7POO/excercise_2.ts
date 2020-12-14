b46fe3716fe56731359189519cb2634faee5b9ca
ILegion <carlos.ksa21@gmail.com>
2020-12-13 18:44:58 -0600

    Excercise 2 POO

---
+++ 7POO/excercise_2.ts
@@ -0,0 +1,9 @@
+// CAL CENTER
+// Imagine you have a call center with three levels of employeers:
+// respondent, manager, and director
+// An incoming telophone call must be first allocated to a respondent who is free.
+// If the respondent can't handle the call. he or she must escalate the call to a manager.
+// If the manager is not free or not able to handle it. Then the call should be escalated to a director.
+// Design the classes and data structures for this problem.
+// Implement a method dispatchCall() which assign a call to the first available employee
+
